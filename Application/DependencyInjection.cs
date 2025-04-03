a30c4de215a23198d8c193000360e85be24a0922
Guilherme <guilhermeteixeira10.gt@gmail.com>
2025-04-03 20:37:55 -0300

    Configurando pipelinebehaviour para captura de erros

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+        ConfigureValidators(services);
+        return services;
+    }
+
+    private static void ConfigureValidators(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+                .AddFluentValidationAutoValidation();
+    }
+}
