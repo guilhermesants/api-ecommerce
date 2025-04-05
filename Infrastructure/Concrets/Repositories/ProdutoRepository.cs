dd064e93934ac8d520daf472e236ce2d07feaf4f
Guilherme <guilhermeteixeira10.gt@gmail.com>
2025-04-05 11:48:05 -0300

    Implementacao de paginacao no endpoint de obter produtos

--- Infrastructure/Concrets/Repositories/ProdutoRepository.cs
+++ Infrastructure/Concrets/Repositories/ProdutoRepository.cs
@@ -2,6 +2,7 @@
 using Domain.Interfaces.Repositories;
 using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Infrastructure.Concrets.Repositories;
 
@@ -13,12 +14,14 @@ public class ProdutoRepository : RepositoryBase<Produto>, IProdutoRepository
     public async Task<Produto?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
         => await Find(x => x.Id == id).FirstOrDefaultAsync(cancellationToken);
 
-    public async Task<IEnumerable<Produto>> ObterProdutosPorFiltroAsync(string? categoria, 
-                                                                        decimal? precoMinimo, 
-                                                                        decimal? precoMaximo, 
-                                                                        bool? ativo, 
-                                                                        CancellationToken cancellationToken)
-    {
+    public async Task<(IEnumerable<Produto> produtos, int totalItens)> ObterProdutosPorFiltroAsync(string? categoria, 
+                                                                                                    decimal? precoMinimo, 
+                                                                                                    decimal? precoMaximo, 
+                                                                                                    bool? ativo,
+                                                                                                    int pagina,
+                                                                                                    int qtdPagina,
+                                                                                                    CancellationToken cancellationToken = default)
+                                {
         var query = GetAll().Include(c => c.Categoria).AsQueryable();
 
         if (!string.IsNullOrWhiteSpace(categoria))
@@ -33,6 +36,13 @@ public class ProdutoRepository : RepositoryBase<Produto>, IProdutoRepository
         if (ativo.HasValue)
             query = query.Where(p => p.Ativo == ativo.Value);
 
-        return await query.ToListAsync(cancellationToken);
+        var total = await query.CountAsync(cancellationToken);
+
+        var produtos = await query
+            .Skip((pagina - 1) * qtdPagina)
+            .Take(qtdPagina)
+            .ToListAsync(cancellationToken);
+
+        return (produtos, total);
     }
 }
